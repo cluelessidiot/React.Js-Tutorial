1423b6051b13b1b8ed2fb23502cf32728813e5c3
AATHIL.TA <cluelessidiot@users.noreply.github.com>
2020-09-08 17:49:47 +0530

    ReactDOM and JSX

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import ReactDOM from 'react-dom';
+ReactDOM.render( < h1 > Hello World < /h1>,document.getElementById("root"))
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+
+        // function App() {
+        //   return (
+        //     <div className="App">
+        //       <header className="App-header">
+        //         <img src={logo} className="App-logo" alt="logo" />
+        //         <p>
+        //           Edit <code>src/App.js</code> and save to reload.
+        //         </p>
+        //         <a
+        //           className="App-link"
+        //           href="https://reactjs.org"
+        //           target="_blank"
+        //           rel="noopener noreferrer"
+        //         >
+        //           Learn React
+        //         </a>
+        //       </header>
+        //     </div>
+        //   );
+        // }
+
+        export default App;
